3d7ec5bc9cd1d7be5b49262ed454a8d48c74ae44
BT_2S14_21 <danielk379@nycstudents.net>
2025-05-19 10:38:36 -0400

    Added hexagon colliders (i had to make a custome model with all manual vericies coordinates), tested out hexagons in game and they work perfectly, i want to be able to make the player move the camera now :)

--- Scripts/HexGrid.gd
+++ Scripts/HexGrid.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
+const HEXA_UNIT := preload("res://Scenes/UnitHex.tscn")
+var grid_size := 13
+var num_levels := 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_create_grid()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _create_grid():
 	pass
+
+	
