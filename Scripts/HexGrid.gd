1803d21489d7de154b7962ac8e06f287293beb72
Ryan <Ryanliu12620@gmail.com>
2025-05-20 10:47:12 -0400

    Did preliminary code in hex grid for a tile map. Then realized im a big dumdum and tried to manually find the values first so i have something to work off of. This included for HexGrid.gd and HexGrid.tscn for testing values. The values are : x = cos(PI/6) [turns out godot works with radians not degrees, found that out after a long time] and y = 0.5
    Worked the information into a tile map generator. Using nested for loops, we can generate a rectangle of connected hexagons along with a small gap between each.

--- Scripts/HexGrid.gd
+++ Scripts/HexGrid.gd
@@ -10,9 +10,36 @@ func _ready():
 	_create_grid()
 
 func _create_grid():
-	for x in range(GRID_SIZE):
-		for y in range(GRID_SIZE):
-			var tile = HEXA_UNIT.instantiate()
+	#var tile = HEXA_UNIT.instantiate()
+	#add_child(tile)
+	#tile.scale = Vector3(1,1,1)
+	#
+	#tile = HEXA_UNIT.instantiate()
+	#add_child(tile)
+	#tile.scale = Vector3(1,1,1)
+	#tile.translate(Vector3(0, 0, 1))
+	#
+	#tile = HEXA_UNIT.instantiate()
+	#add_child(tile)
+	#tile.scale = Vector3(1,1,1)
+	#tile.translate(Vector3(cos(PI/6), 0, 0.5))
+	
+	var x_cord := 0.0
+	var y_cord := 0.0
+	for x in range(GRID_SIZE): # column
+		if (x + 1) % 2 == 0: # every second column, shift the starting y down
+			y_cord = 0.5 # by 0.5
+		for y in range(GRID_SIZE): # rows
+			var tile = HEXA_UNIT.instantiate() # create and add tile
 			add_child(tile)
+			tile.translate(Vector3(x_cord, 0, y_cord)) # translate the tile
+			tile.translate(Vector3(cos(PI/6) / 50, 0, 0.5 / 50)) # create a gap
+			y_cord += 1 * 1.03 # this helps shift the next hexagon down
+			
+		x_cord += cos(PI/6) * 1.03 # this helps connect hexagons in a row
+		y_cord = 0 # reset position
+			
 
 	
+
+
