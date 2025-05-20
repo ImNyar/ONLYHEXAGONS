90defaadbed69efebadbac087dabd44ed1c01a5f
Ryan Liu <ryanliu12620@gmail.com>
2025-05-19 23:30:32 -0400

    Getting started on tile map through gd script, instanciate, and create hexa units as chi;ds of the map

--- Scripts/HexGrid.gd
+++ Scripts/HexGrid.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
 const HEXA_UNIT := preload("res://Scenes/UnitHex.tscn")
-var grid_size := 13
-var num_levels := 8
+const HEXA_SCALE : = 5
+const GRID_SIZE := 13
+const NUM_LEVELS := 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_create_grid()
 
 func _create_grid():
-	pass
+	for x in range(GRID_SIZE):
+		for y in range(GRID_SIZE):
+			var tile = HEXA_UNIT.instantiate()
+			add_child(tile)
 
 	
