f09cdd9daa0875326514cbd53a98846782897a05
BT_2S14_21 <ryanliu12620@gmail.com>
2025-06-03 11:02:19 -0400

    Cropped my models to fit the proper colliders Did a lot of great work on the menu. Got the menu working. Adding signals to make it connect to other menus and the game itself, etc. I want to finish the menu system in one more day and then dedicate everything else to gameplay and multiplayer.

---
+++ Scripts/MenuButtonContainer.gd
@@ -0,0 +1,9 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("start") # Replace with function body.
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/game.tscn") # Replace with function body.
